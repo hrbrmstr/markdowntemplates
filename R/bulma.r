8faff600dfa0210b8567b2f7495854b9b96caa4b
Bob Rudis <bob@rudis.net>
2016-02-06 06:33:34 -0500

    self_contained=FALSE now works like it should

--- R/bulma.r
+++ R/bulma.r
@@ -7,32 +7,51 @@
 #' @inheritParams rmarkdown::html_document
 #'
 #' @export
-bulma <- function(toc = FALSE, toc_depth = 3, number_sections = FALSE,
-                    fig_width = 7, fig_height = 5, fig_retina = if (!fig_caption) 2,
-                    fig_caption = FALSE, dev = "png", smart = TRUE, self_contained = TRUE,
-                    theme = NULL, highlight = "tango", mathjax = "default",
-                    template = "default", css = NULL, includes = NULL, keep_md = TRUE,
-                    lib_dir = NULL, md_extensions = NULL, pandoc_args = NULL,
-                    ...) {
+bulma <- function(number_sections = FALSE,
+                  fig_width = 7,
+                  fig_height = 5,
+                  fig_retina = if (!fig_caption) 2,
+                  fig_caption = FALSE,
+                  dev = 'png',
+                  smart = TRUE,
+                  self_contained = TRUE,
+                  highlight = "default",
+                  mathjax = "default",
+                  extra_dependencies = NULL,
+                  css = NULL,
+                  includes = NULL,
+                  keep_md = FALSE,
+                  lib_dir = NULL,
+                  md_extensions = NULL,
+                  pandoc_args = NULL,
+                  ...) {
 
-  css <- c(css, list.files(system.file("rmarkdown", "templates", "bulma", "resources",
-                                       package = "markdowntemplates"),
-                           "*.css$", full.names=TRUE))
+  toc <- FALSE
+  toc_depth <- 3
+  theme <- NULL
+  template <- "default"
+  code_folding <- "none"
+
+  dep <- htmltools::htmlDependency("bulmastyle", "0.0.10",
+           system.file("rmarkdown", "templates", "bulma", "resources",
+                       package = "markdowntemplates"),
+           stylesheet=c("bulma.css", "CUSTOMIZE_ME.css"))
+
+  extra_dependencies <- append(extra_dependencies, list(dep))
 
   args <- c("--standalone")
   args <- c(args, "--section-divs")
   args <- c(args, rmarkdown::pandoc_toc_args(toc, toc_depth))
 
-  if (identical(template, "default"))
-    args <- c(args, "--template",
-              rmarkdown::pandoc_path_arg(system.file("rmarkdown", "templates", "bulma", "base.html",
-                                                     package = "markdowntemplates")))
-  else if (!is.null(template))
-    args <- c(args, "--template", rmarkdown::pandoc_path_arg(template))
+  args <- c(args, "--template",
+            rmarkdown::pandoc_path_arg(system.file("rmarkdown", "templates", "bulma", "base.html",
+                                                   package = "markdowntemplates")))
 
-  if (number_sections) args <- c(args, "--number-sections")
+  if (number_sections)
+    args <- c(args, "--number-sections")
 
-  for (css_file in css) args <- c(args, "--css", rmarkdown::pandoc_path_arg(css_file))
+  for (css_file in css)
+    args <- c(args, "--css", rmarkdown::pandoc_path_arg(css_file))
 
   pre_processor <- function(metadata, input_file, runtime,
                             knit_meta, files_dir, output_dir) {
@@ -50,20 +69,22 @@ bulma <- function(toc = FALSE, toc_depth = 3, number_sections = FALSE,
 
   }
 
-  output_format(knitr = rmarkdown::knitr_options_html(fig_width, fig_height,
-                                                      fig_retina, keep_md, dev),
-                pandoc = rmarkdown::pandoc_options(to = "html",
-                                                   from = from_rmarkdown(fig_caption,
-                                                                         md_extensions),
-                                                   args = args),
-                keep_md = keep_md, clean_supporting = self_contained,
-                pre_processor = pre_processor,
-                base_format = rmarkdown::html_document_base(smart = smart,
-                                                            theme = theme,
-                                                            self_contained = self_contained,
-                                                            lib_dir = lib_dir,
-                                                            mathjax = mathjax,
-                                                            template = template,
-                                                            pandoc_args = pandoc_args,
-                                                            ...))
+  output_format(
+    knitr = rmarkdown::knitr_options_html(fig_width, fig_height,
+                                          fig_retina, keep_md, dev),
+    pandoc = rmarkdown::pandoc_options(to = "html",
+                                       from = from_rmarkdown(fig_caption,
+                                                             md_extensions),
+                                       args = args),
+    keep_md = keep_md, clean_supporting = self_contained,
+    pre_processor = pre_processor,
+    base_format = rmarkdown::html_document_base(smart = smart,
+                                                theme = theme,
+                                                self_contained = self_contained,
+                                                lib_dir = lib_dir,
+                                                mathjax = mathjax,
+                                                template = template,
+                                                pandoc_args = pandoc_args,
+                                                extra_dependencies = extra_dependencies,
+                                                ...))
 }
--- R/markdowntemplates-package.R
+++ R/markdowntemplates-package.R
@@ -1,7 +1,29 @@
-#' Personal R markdown templates
+#' Alternate R markdown templates
+#'
+#' These are a set of alternate R markdown templates that do not use Bootstrap
+#' and do not include or rely on jQuery. These are primarily here for
+#' demonstration purposes but are also useful in and of themselves (it's always
+#' good to have choices). If you have a particular framework you like and it's
+#' not here, file an issue and I'll add it.
+#'
+#' `bulma` (`output: markdowntemplates::bulma`) uses the [Bulma CSS
+#' framework](http://bulma.io) and has optional `navtitle` and `navlink` YAML
+#' header elements which will be used in header of the generated file.
+#'
+#' `skeleton` (`output: markdowntemplates::skeleton`) or `default` (`output:
+#' markdowntemplates::default`) uses the [Skeleton CSS
+#' framework](http://getskeleton.com) and also has optional `navtitle` and
+#' `navlink` YAML header elements which will be used in header of the generated
+#' file.
+#'
+#' `minimal` (`output: markdowntemplates::minimal`) uses no CSS framework and
+#' only uses the `title` YAML header element.
+#'
+#' When you use RStudio to create a new R Markdown document, select "From
+#' Template" and choose one of these templates.
 #'
 #' @name markdowntemplates
 #' @docType package
 #' @author Bob Rudis (@@hrbrmstr)
-#' @import rmarkdown knitr
+#' @import rmarkdown knitr htmltools
 NULL
