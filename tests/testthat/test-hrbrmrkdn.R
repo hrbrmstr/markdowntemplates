399d201502f6fbcccba0c9622ee468ee0591933d
Bob Rudis <bob@rudis.net>
2016-02-06 10:52:08 -0500

    basic template tests

--- tests/testthat/test-hrbrmrkdn.R
+++
@@ -1,6 +0,0 @@
-context("basic functionality")
-test_that("we can do something", {
-
-  #expect_that(some_function(), is_a("data.frame"))
-
-})
---
+++ tests/testthat/test-markdowntemplates.R
@@ -0,0 +1,54 @@
+context("template knitting works")
+test_that("bulma template HTML output is correct", {
+
+  # TODO check more things
+
+  tmp <- tempfile(fileext=".html")
+  rmarkdown::render("bulma.Rmd", clean=TRUE, quiet=TRUE, output_file=tmp)
+  pg <- xml2::read_html(tmp)
+  expect_that(length(rvest::html_nodes(pg, "meta[property='og:title']")),
+              is_more_than(0))
+  expect_that(rvest::html_text(rvest::html_nodes(pg, "title")),
+              equals("INSERT_TITLE_HERE"))
+  unlink(tmp)
+
+})
+
+test_that("kube template HTML output is correct", {
+
+  # TODO check more things
+
+  tmp <- tempfile(fileext=".html")
+  rmarkdown::render("kube.Rmd", clean=TRUE, quiet=TRUE, output_file=tmp)
+  pg <- xml2::read_html(tmp)
+  expect_that(rvest::html_text(rvest::html_nodes(pg, "title")),
+              equals("INSERT_TITLE_HERE"))
+  unlink(tmp)
+
+})
+
+test_that("minimal template HTML output is correct", {
+
+  # TODO check more things
+
+  tmp <- tempfile(fileext=".html")
+  rmarkdown::render("minimal.Rmd", clean=TRUE, quiet=TRUE, output_file=tmp)
+  pg <- xml2::read_html(tmp)
+  expect_that(rvest::html_text(rvest::html_nodes(pg, "title")),
+              equals("INSERT_TITLE_HERE"))
+  unlink(tmp)
+
+})
+
+test_that("skeleton template HTML output is correct", {
+
+  # TODO check more things
+
+  tmp <- tempfile(fileext=".html")
+  rmarkdown::render("skeleton.Rmd", clean=TRUE, quiet=TRUE, output_file=tmp)
+  pg <- xml2::read_html(tmp)
+  expect_that(rvest::html_text(rvest::html_nodes(pg, "title")),
+              equals("INSERT_TITLE_HERE"))
+  unlink(tmp)
+
+})
