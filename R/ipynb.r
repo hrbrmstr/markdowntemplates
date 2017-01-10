014e8a6a2c2cd5edee5ecd46eaae2d9829f538ea
Bob Rudis <bob@rudis.net>
2017-01-10 07:16:38 -0500

    messages

--- R/ipynb.r
+++ R/ipynb.r
@@ -3,18 +3,26 @@
 #' @export
 to_jupyter <- function(inputFile, encoding) {
 
+  message("Started converting R Markdown to Jupyter Notebook...")
   output_file <-  sprintf("%s.%s", tools::file_path_sans_ext(inputFile), "ipynb")
 
   tf <- tempfile(fileext=".Rmd")
 
+  message(" - stripping YAML header")
   tmp <- readLines(inputFile)
   yaml_end <- which(grepl("^---", tmp))[2]
   writeLines(tmp[(yaml_end+1):length(tmp)], tf)
 
+  message(" - running notedown...")
   system2("notedown", args=c(tf, "--rmagic", "--run"), stdout=output_file)
+
+  message("Completed conversion.")
+
+  rstudioapi::navigateToFile(output_file)
 
   unlink(tf)
 
-  output_file
+  cat(sprintf("Output file is at: [%s]", output_file))
+  invisible(output_file)
 
 }
