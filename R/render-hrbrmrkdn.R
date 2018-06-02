36f2cdf6cad99992ebc6a8de475a87ff353db662
boB Rudis <bob@rud.is>
2018-06-02 03:38:04 -0400

    removed dbg code; fixed collapse; updated prism

--- R/render-hrbrmrkdn.R
+++ R/render-hrbrmrkdn.R
@@ -1,9 +1,15 @@
 chunk_hook_html = function(x, options) {
 
-  cat(x, file="/tmp/a.txt", append=TRUE)
+  # cat(x, file="/tmp/a.txt", append=TRUE)
+  # cat("\n\n=================\n\n", file="/tmp/a.txt", append=TRUE)
 
   if (knitr:::output_asis(x, options)) return(x)
 
+  fence_char <- '`'
+  fence <- paste(rep(fence_char, 3), collapse = '')
+  if (isTRUE(options$collapse)) {
+    x = gsub(paste0('\n([', fence_char, ']{3,})\n+\\1(', tolower(options$engine), ')?\n'), "\n", x)
+  }
   x <- sprintf('<div class="chunk" id="%s"><div class="rcode">%s</div></div>', options$label, x)
   x <- gsub('<div class="rcode">\\s*</div>', '', x) # rm empty rcode layers
   x <- gsub('<div class="rcode">', sprintf('<div class="language-%s">', tolower(options$engine)), x)
